215b48184f66393129078322f5f754ee8783312d
rajarshi700 <113887759+rajarshi700@users.noreply.github.com>
2026-07-15 12:35:39 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
+export const ServerUrl  = "https://interviewiqbackend-h6yh.onrender.com"
 
 function App() {
 
