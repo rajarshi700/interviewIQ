002d62db2328e159a9349a9c7a574fcb5daf1b05
rajarshi700 <retet2022@gmail.com>
2026-07-15 22:38:06 +0530

    change domain name

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"https://interviewiqfrontend.onrender.com",
+    origin:"https://ai-hire-booster.onrender.com",
     credentials:true
 }))
 
